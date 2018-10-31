2f93b6defbfd463d4c8a69d3f5a4020ef5503b76
Idris Karaali <30285751+realka@users.noreply.github.com>
2018-10-31 23:07:25 +0300

    Update csvbuilder.py

--- csvbuilder.py
+++ csvbuilder.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import glob, os
-from PIL import Image
-import fnmatch
 import numpy as np
 
-arr1 = np.empty((2,300*6), dtype=object)
-#name = "retinex"
-tips ="/train/"
-counter = 0
-for infile in glob.glob("path/csvbuilder"+tips+"/*.jpg", recursive=True): 
-    file, ext = os.path.splitext(infile)
-    im = Image.open(infile)
-    #arr.append(im.split("/"))
-    a0,a1,a2,a3,a4,a5,a6,a7,a8=file.split("/")
-    t1,t2=a8.split("\\")
-    t3=("images"+tips+t1+"/"+t2+".jpg")
-    print(t1+"----"+t3)
-    arr1[0,counter] = t3
-    arr1[1,counter] = t1
-    counter = counter + 1
-results = pd.DataFrame({'img':arr1[1,:] ,'label':arr1[0,:]})
-results.to_csv('train.csv', index=False) #simple output (retinex1-Q3-A.jpg)
+# Variables Definition
+arr1 = np.empty((2,300*6), dtype=object) # 300 image per class, 6 class
+tip =['Gtrain','Gtest']
+add =['Q3A','Q3B','Q3C','Q4A','Q4B','Q4C']
+
+for tips in tip:
+    counter = 0
+    for ad in add:
+        for infile in glob.glob(tips+"/"+ad+"/*.jpg", recursive=True):
+            # Split Path
+            file, ext = os.path.splitext(infile)
+            fold,name =file.split("\\")
+            print("./"+ad+"/"+name+ext+"----"+ad)
+            arr1[0,counter] = "./"+ad+"/"+name+ext
+            arr1[1,counter] = ad
+            counter = counter + 1
+    # Export to CSV
+    results = pd.DataFrame({'label':arr1[1,:] ,'img':arr1[0,:]})
+    results.to_csv(tips+'.csv', index=False)
