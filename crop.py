5d96f7eda3060ac101aac62f582d1359c5ec5571
Ka Software <30285751+realka@users.noreply.github.com>
2019-02-14 13:07:23 +0300

    Update crop.py

--- crop.py
+++ crop.py
@@ -10,9 +10,6 @@ counter_height = 0
 while img.width > sizewidth:
     sizeheight = counter_height * 300
     left, top, width, height = sizewidth, sizeheight, sizewidth + 300, sizeheight + 300
-    #cropped = img.crop( ( left, top, width, height ) )
-    #cropped.save(str(counter)+'-Q3-C.jpg')
-    #counter = counter + 1
     while img.height > sizeheight: 
         left, top, width, height = sizewidth, sizeheight, sizewidth + 300 , sizeheight + 300
         cropped = img.crop( ( left, top, width, height ) )
