f70be82f4c64867a127fba085b09c659d03ab890
Ka Software <30285751+realka@users.noreply.github.com>
2019-02-14 13:31:14 +0300

    Update RandomImageSelectForTest.py

--- RandomImageSelectForTest.py
+++ RandomImageSelectForTest.py
@@ -3,12 +3,15 @@ from PIL import Image
 import os
 
 
+
+#path = "filtered/"
+#folder = ['2x2Closing','2x2Dilation','2x2Erosion+Dilation','2x2Erosion','2x2Opening',
+#          '4x4Closing','4x4Dilation','4x4Erosion','4x4Opening','Bilateral','Blur',
+#          'GaussianBlur','GreyMedian','Median','sepFilter']
 arr = range(350)
-path = "filtered/"
+path = "data/"
 rand = random.sample(arr,50)
-folder = ['2x2Closing','2x2Dilation','2x2Erosion+Dilation','2x2Erosion','2x2Opening',
-          '4x4Closing','4x4Dilation','4x4Erosion','4x4Opening','Bilateral','Blur',
-          'GaussianBlur','GreyMedian','Median','sepFilter']
+folder =['OriginalData']
 subfolder = ['Q3A','Q3B','Q3C','Q4A','Q4B','Q4C']
 for fold in folder:
     for ad in subfolder:
