89004df9fbc5c408323bb279afb23ccc216deac6
Ka Software <30285751+realka@users.noreply.github.com>
2019-02-24 01:06:39 +0300

    Update Renamer.py

--- Renamer.py
+++ Renamer.py
@@ -1,12 +1,13 @@
-
 from PIL import Image
 import glob, os
-name="explainer.jpg"
-counter = 1
+import sys
+#name="explainer.jpg"
+name= sys.argv[1]
+counter = 351
 for infile in glob.glob("*.jpg"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
-    im.save(str(counter)+name+".jpg")
+    im.save(name+str(counter)+".jpg")
     counter = counter + 1
-    
+
 
