cba8f1ce6b411a1a2ab8ab2fe66f123417de4056
Ka Software <30285751+realka@users.noreply.github.com>
2021-01-11 15:58:07 +0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -10,13 +10,13 @@ def variance_of_laplacian(image):
     return cv2.Laplacian(image, cv2.CV_64F).var()
 
 subfolder =['Q3A','Q3B','Q3C','Q4A','Q4B','Q4C']
-for ad in subfolder:
+for name in subfolder:
     for x in range(350):
 
-        image = cv2.imread("Train/"+ad+"/"+ad+"."+str(x+1)+".jpg")
+        image = cv2.imrename("Train/"+name+"/"+name+"."+str(x+1)+".jpg")
         #Median#
         #Median#median = cv2.medianBlur(image,5)
-        #Median#plt.imsave("Mediantrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", median,cmap = 'gray')
+        #Median#plt.imsave("Mediantrain/"+name+"/"+name+"."+str(x+1)+".jpg", median,cmap = 'gray')
 
         #Gabor#
         #Gabor#kernel = cv2.getGaborKernel((15, 15), 2, 0, 20, 1, 10, cv2.CV_32F)
@@ -27,22 +27,22 @@ for ad in subfolder:
         #Gabor#filtered = filtered.astype(np.float32)
         #Gabor#filtered = filtered.astype(np.float32) * 255
         #Gabor#filtered = filtered.astype(np.float32)
-        #Gabor#status = cv2.imwrite("Gtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", filtered)
+        #Gabor#status = cv2.imwrite("Gtrain/"+name+"/"+name+"."+str(x+1)+".jpg", filtered)
         #Gabor#print(status)
 
         #bilateralFilter
         #bilateralFilter#bilateral = cv2.bilateralFilter(image,5,150,150)
-        #bilateralFilter#image = cv2.addWeighted(image, 1.5, bilateral, -0.5, 0)
+        #bilateralFilter#image = cv2.namedWeighted(image, 1.5, bilateral, -0.5, 0)
         #bilateralFilter#image = cv2.bilateralFilter(image, 5, 150, 150)
-        #bilateralFilter#status = cv2.imwrite("Bilateraltrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", image)
+        #bilateralFilter#status = cv2.imwrite("Bilateraltrain/"+name+"/"+name+"."+str(x+1)+".jpg", image)
 
         #blur
         #blur# blur = cv2.blur(image,(5,5))
-        #blur# status = cv2.imwrite("Blurtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", blur)
+        #blur# status = cv2.imwrite("Blurtrain/"+name+"/"+name+"."+str(x+1)+".jpg", blur)
 
         #gaussianBlur
         #gaussianBlur# gblur = cv2.GaussianBlur(image,(5,5),0)
-        #gaussianBlur# status = cv2.imwrite("GaussianBlurtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", gblur)
+        #gaussianBlur# status = cv2.imwrite("GaussianBlurtrain/"+name+"/"+name+"."+str(x+1)+".jpg", gblur)
 
         #boxFilter
         # image=100*(image-np.mean(image))
@@ -52,26 +52,26 @@ for ad in subfolder:
         # image=cv2.boxFilter(image,-1,(15,15))
         # image[np.where(image>0)]=255
         # image = image.astype(np.float32) / 255
-        # status = cv2.imwrite("Boxedtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", image)
+        # status = cv2.imwrite("Boxedtrain/"+name+"/"+name+"."+str(x+1)+".jpg", image)
 
         #erosion
         # kernel = np.ones((2,2),np.uint8)
-        # erosion = cv2.erode(image,kernel,iterations = 1) # fosilleri ve delikleri buluyor
-        # dilation = cv2.dilate(erosion,kernel,iterations = 1)  #damarları buluyor
-        # opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel) # fosilleri ve delikleri buluyor
-        # closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel) #damarları buluyor
-        # gradient = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
+        # erosion = cv2.erode(image,kernel,iterations = 1) # Finds fossils and holes
+        # dilation = cv2.dilate(erosion,kernel,iterations = 1)  # finds veins
+        # opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel) # Finds fossils and holes
+        # closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel) # finds veins
+        # grnameient = cv2.morphologyEx(image, cv2.MORPH_GRnameIENT, kernel)
         # tophat = cv2.morphologyEx(image, cv2.MORPH_TOPHAT, kernel)
         #blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
-        # status = cv2.imwrite("2x2Erosion+Dilation_Train/"+ad+"/"+ad+"."+str(x+1)+".jpg", dilation)
-        # status = cv2.imwrite("4x4Dilationtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", dilation)
-        # status = cv2.imwrite("4x4Openingtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", opening)
-        # status = cv2.imwrite("4x4Closingtrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", closing)
+        # status = cv2.imwrite("2x2Erosion+Dilation_Train/"+name+"/"+name+"."+str(x+1)+".jpg", dilation)
+        # status = cv2.imwrite("4x4Dilationtrain/"+name+"/"+name+"."+str(x+1)+".jpg", dilation)
+        # status = cv2.imwrite("4x4Openingtrain/"+name+"/"+name+"."+str(x+1)+".jpg", opening)
+        # status = cv2.imwrite("4x4Closingtrain/"+name+"/"+name+"."+str(x+1)+".jpg", closing)
 
         # sepFilter2D
         # kernel = cv2.getGaussianKernel( 15, 2 )
         # twoD = cv2.sepFilter2D(image, -1, kernel, kernel)
-        # status = cv2.imwrite("sepFiltertrain/"+ad+"/"+ad+"."+str(x+1)+".jpg", twoD)
+        # status = cv2.imwrite("sepFiltertrain/"+name+"/"+name+"."+str(x+1)+".jpg", twoD)
 
         # Laplacian,Sobelx,Sobely test
         # laplacian = cv2.Laplacian(image,cv2.CV_64F)
